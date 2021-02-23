1c2207a4a5534779bb516476611c901a5716e089
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-23 16:59:31 -0600

    run rubocop

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
-    def index
-        articles = Article.recent
-        render json: serializer.new(articles), status: :ok
-    end
+  def index
+    articles = Article.recent
+    render json: serializer.new(articles), status: :ok
+  end
 
-    def serializer
-        ArticleSerializer
-    end
+  def serializer
+    ArticleSerializer
+  end
 end
