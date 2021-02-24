1d0bca77c4c9e5b8d9fa21d839857f6ee3f2c588
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-24 15:37:18 -0600

    add test for user validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    login { "MyString" }
-    name { "MyString" }
-    url { "MyString" }
-    avatar_url { "MyString" }
-    provider { "MyString" }
+    sequence(:login) { |n| "fivan#{n}" }
+    name "Ivancito"
+    url "http://example.com"
+    avatar_url "http://example.com/avatar"
+    provider "github"
   end
 end
