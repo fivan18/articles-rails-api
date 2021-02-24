a3216497e2cb29db974d9e0b80fecaac8cc3a4de
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-24 15:53:56 -0600

    fix rubocop errors

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     sequence(:login) { |n| "fivan#{n}" }
-    name "Ivancito"
-    url "http://example.com"
-    avatar_url "http://example.com/avatar"
-    provider "github"
+    name { 'Ivancito' }
+    url { 'http://example.com' }
+    avatar_url { 'http://example.com/avatar' }
+    provider { 'github' }
   end
 end
