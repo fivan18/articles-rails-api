9c6bcdf2e2bdd5b733f095fcf6afc92515be5d3f
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-25 14:21:41 -0600

    add access token

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -1,7 +1,7 @@
 class UserAuthenticator
   class AuthenticationError < StandardError; end
 
-  attr_reader :user
+  attr_reader :user, :access_token
 
   def initialize(code)
     @code = code
@@ -11,6 +11,12 @@ class UserAuthenticator
     raise AuthenticationError if token.try(:error).present?
 
     prepare_user
+
+    @access_token = if user.access_token.present?
+                      user.access_token
+                    else
+                      user.create_access_token
+                    end
   end
 
   private
