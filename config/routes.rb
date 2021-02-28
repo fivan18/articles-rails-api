987118ff8ad63770bfea5177bbec543f91bb14de
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-02-28 11:23:25 -0600

    add routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # get '/articles', to: 'articles#index'
   post 'login', to: 'access_tokens#create'
   delete 'logout', to: 'access_tokens#destroy'
-  resources :articles
+
+  resources :articles do
+    resources :comments, only: %i[index create]
+  end
 end
